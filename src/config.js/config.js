d945d51a5cfcd18e274a6e3a15c86db2f4dc93cc
Abdulla Al Samin <abdullahalsamin79@gmail.com>
2026-04-10 22:09:15 -1200

    update url

--- src/config.js/config.js
+++ src/config.js/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://127.0.0.1:8000";  // your Django backend URL
-export const BASE_URL = "https://local-market-backend.onrender.com";  // your Django backend URL
+export const BASE_URL = "https://local-mart-11yd.onrender.com"; // your Django backend URL
